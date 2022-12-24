ce75a0785896aa7775dc0ad466ae92543916fbd4
MohammedAlaaM <121355281+MohammedAlaaM@users.noreply.github.com>
2022-12-24 23:36:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System; 
+using System.Linq; 
+ 
+namespace Prime_Numbers 
+{ 
+ public static class Program 
+ { 
+  public static void Main(string[] args) 
+  { 
+   Console.WriteLine("Enter A Number 1"); 
+   int Number1 = int.Parse(Console.ReadLine()); 
+    
+   Console.WriteLine("Enter A Number 2"); 
+   int Number2 = int.Parse(Console.ReadLine()); 
+    
+   Console.WriteLine(" Theses are the numbers , Number1 , Number2"); 
+   for(int M = Number1 ; M <= Number2 ; M++) 
+    
+ 
+    
+    
+    
+ 
+   { 
+     
+     
+    int counter = 0 ; 
+    for (int A = 2 ; A<= M/2; A++ ) 
+ 
+    { 
+     if(M % A == 0) 
+     
+    { 
+     counter++; 
+     break; 
+     
+    } 
+     
+ 
+   } 
+      if (counter == 0 && M != 1) 
+    
+ 
+ 
+          { 
+              Console.WriteLine("{0}", M ) ; 
+ 
+} 
+} 
+}
+}
+}
